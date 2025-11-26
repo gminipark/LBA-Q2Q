cf428617a472d97e0b12b11c64b96a1ec74359a4
gminipark <pgm1219@khu.ac.kr>
2025-11-26 07:37:51 +0000

    add lora merge script

---
+++ 2025/ICQA/FT/LLaVA/scripts/merge.sh
@@ -0,0 +1,4 @@
+python scripts/merge_lora_weights.py \
+    --model-path "checkpoints/llava-v1.5-13b-checker-lora-2e-4-te/checkpoint-4896" \
+    --model-base "liuhaotian/llava-v1.5-13b" \
+    --save-model-path "checkpoints/llava-v1.5-13b-checker-2e-4-te/checkpoint-4896-merged" \
